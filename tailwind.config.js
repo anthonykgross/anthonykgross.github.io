a15be0dc73ea457e2a64cc50cf534d8135a363cd
Anthony K GROSS <anthony.k.gross@gmail.com>
2021-12-01 17:16:06 +0100

    Update

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,7 +22,8 @@ module.exports = {
         }
       },
       fontFamily: {
-        'fjalla': ['Fjalla One'],
+        'jenna-sue': ['JennaSue'],
+        'hello-world': ['HelloWorld'],
       }
     }
   },
